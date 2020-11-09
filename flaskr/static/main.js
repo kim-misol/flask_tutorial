c29ef3c20106e383439f28cd4065a6d54abaa945
kim-misol <wonderfulpine@gmail.com>
2020-11-09 11:45:26 +0900

    Fixed request get data issue

--- flaskr/static/main.js
+++ flaskr/static/main.js
@@ -22,11 +22,11 @@ let quill = new Quill('#editor-container', {
   modules: {
     toolbar: toolbarOptions
   },
-  placeholder: 'Type Content...',
+  placeholder: 'Type Content..',
   theme: 'snow'  // or 'bubble'
 });
 
-//document.querySelector('.create-post').addEventListener('click', createPostEvent);
+document.querySelector('.create-post').addEventListener('click', createPostEvent);
 
 function createPostEvent(event) {
     let form = document.querySelector('form');
@@ -43,57 +43,10 @@ function createPostEvent(event) {
       console.log(value);
     }
 
-//    fetch(window.createPostUrl, {
-//      headers: {
-//        "Content-Type": "multipart/form-data"
-//      },
-//      method: 'POST', // or 'PUT'
-//      body: formData // data can be `string` or {object}!
-//    }).then(res => res.json())
-//    .then(response => console.log('Success:', JSON.stringify(response)))
-//    .catch(error => console.error('Error:', error));
-
     fetch(window.createPostUrl, {
-      mode: 'cors',
       method: 'POST', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: JSON.stringify(formData)
+      body: formData
     }).then(res => res.json())
     .then(response => console.log('Success:', JSON.stringify(response)))
     .catch(error => console.error('Error:', error));
 }
-
-$(document).ready(function() {
-    $('.create-post').click(function() {
-        console.log('clicked submit button');
-        let form = document.querySelector('form');
-        let formData = new FormData(form);
-        let delta = quill.getContents();
-        let text = quill.getText();
-        formData.append('content_json', JSON.stringify(delta));
-        formData.append('content', text);
-        for (var value of formData.values()) {
-          console.log(value);
-        }
-        $.ajax({
-            url: window.createPostUrl,
-            type: 'POST',
-            contentType: 'application/json; charset=utf-8',
-            dataType:'JSON',
-            data: JSON.stringify(formData),
-            processData: false,
-            success: function(d) {
-                if (d.success) {
-                    alert('Success');
-                } else {
-                    alert("Fail - " + d.message);
-                }
-            },
-            failure: function(err) {
-                console.log(err);
-            }
-        });
-    });
-});
